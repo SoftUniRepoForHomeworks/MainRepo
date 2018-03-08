666343e3308d0da61246ee003bc9a6580dc1fa94
StilianKuhev <37152295+StilianKuhev@users.noreply.github.com>
2018-03-08 16:04:36 +0200

    Update exersice.cs

--- Exersice one/exersice.cs
+++ Exersice one/exersice.cs
@@ -1,2 +1,23 @@
-//TO DO
-//1 -> make the file done and push it in git
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Globalization;
+using System.Numerics;
+using System.Text;
+
+namespace currentProjectForSolvingTest
+{
+	class MainClass
+	{
+		public static void Main ()
+		{
+			int firstNum = int.Parse (Console.ReadLine ());
+			int secondNum = int.Parse (Console.ReadLine ());
+			int thirdNum = int.Parse (Console.ReadLine ());
+			int forthNum = int.Parse (Console.ReadLine ());
+
+			Console.WriteLine ($"{firstNum:D4} {secondNum:D4} {thirdNum:D4} {forthNum:D4}");
+		}
+	}
+}

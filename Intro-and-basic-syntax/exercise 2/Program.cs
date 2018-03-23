21cf0f14858871c57db8c066d8251419bb4dcced
George <georgi.nanev@abv.bg>
2018-03-23 20:17:25 +0200

    adding exercise 2/Program.cs

---
+++ Intro-and-basic-syntax/exercise 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace modul2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double one = double.Parse(Console.ReadLine());
+            double two = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{one * two:f2}");
+        }
+    }
+}
